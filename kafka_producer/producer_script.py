04f5df3977037b4d8579ec8dc2c4621b188d547f
Stefan Stimmer <stefan.stimmer@f-i-ts.de>
2026-02-09 18:43:29 +0100

    added try to producer

--- kafka_producer/producer_script.py
+++ kafka_producer/producer_script.py
@@ -9,21 +9,24 @@ producer = Producer({
     "bootstrap.servers": BOOTSTRAP,
 })
 
-with open(CSV_PATH, newline="", encoding="utf-8") as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        event = {
-            "location": row["Location"],
-            "timestamp": row["Date_Time"],
-            "temperature_c": float(row["Temperature_C"]),
-            "humidity_pct": float(row["Humidity_pct"]),
-            "precipitation_mm": float(row["Precipitation_mm"]),
-            "wind_speed_kmh": float(row["Wind_Speed_kmh"]),
-        }
+try:
+    with open(CSV_PATH, newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            event = {
+                "location": row["Location"],
+                "timestamp": row["Date_Time"],
+                "temperature_c": float(row["Temperature_C"]),
+                "humidity_pct": float(row["Humidity_pct"]),
+                "precipitation_mm": float(row["Precipitation_mm"]),
+                "wind_speed_kmh": float(row["Wind_Speed_kmh"]),
+            }
 
-        producer.produce(topic = TOPIC, 
-                         value = json.dumps(event).encode("utf-8"))
-        producer.poll(0)
+            producer.produce(topic = TOPIC, 
+                             value = json.dumps(event).encode("utf-8"))
+            producer.poll(0)
 
-producer.flush()
-print("done")
+    producer.flush()
+    print("done")
+except:
+    print(Exception)
